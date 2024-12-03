ffe24820fd583ec45e15a271610f01aa6fb1cb64
Yuki Watanabe <ukwhatn@gmail.com>
2024-12-04 03:56:36 +0900

    st20

--- resources/views/sheets/reserve.blade.php
+++ resources/views/sheets/reserve.blade.php
@@ -1,9 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="card mt-5">
-        <div class="card-body align-self-center">スクリーン1</div>
-    </div>
     <div class="row row-cols-5 mt-5">
         @foreach ($sheets as $key => $sheet)
             <div class="col">
